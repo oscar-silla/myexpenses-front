e68db0f83c8b45fd4f4d2bb0b0cf6396ea0718e7
oscar <oscarsilla00@gmail.com>
2025-01-23 18:54:44 +0100

    fix: Show transactions by type

--- src/app/components/transactions-list/transactions-list.component.ts
+++ src/app/components/transactions-list/transactions-list.component.ts
@@ -10,18 +10,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { TransactionDate } from '../../types/models/response/transaction-date/transaction-date.type';
 import { TransactionDateResponse } from '../../types/models/response/transaction-date/transaction-date-response.type';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-transactions-list',
   standalone: true,
-  imports: [
-    MatListModule,
-    MatDividerModule,
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [MatListModule, MatDividerModule, RouterLink],
   templateUrl: './transactions-list.component.html',
   styleUrl: './transactions-list.component.css',
 })
@@ -43,5 +37,19 @@ export class TransactionsListComponent implements OnInit {
         console.log('Fetching expenses complete');
       },
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['type'] && changes['type'].currentValue) {
+      this.type = changes['type'].currentValue;
+    }
+  }
+
+  getTransactionDate(): TransactionDate[] {
+    return this.transactionDates.filter((date) =>
+      this.type === 'EXPENSE'
+        ? date.expenses.length > 0
+        : date.revenues.length > 0
+    );
   }
 }
