464338115319a5468b5af456fe73fbc45ea69d1f
oscar <oscarsilla00@gmail.com>
2025-01-28 20:12:51 +0100

    feat: Transaction date amount

--- src/app/types/models/response/transaction-date/transaction-date.type.ts
+++ src/app/types/models/response/transaction-date/transaction-date.type.ts
@@ -1,7 +1,9 @@
 import { Transaction } from '../../../domain/transaction.type';
+import { TransactionDateAmount } from '../tansaction-date-amount/transaction-date-amount.type';
 
 export type TransactionDate = {
   date: Date;
   expenses: Transaction[];
   revenues: Transaction[];
+  amount: TransactionDateAmount;
 };
