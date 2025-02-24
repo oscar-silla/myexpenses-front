8edaebfb1e41da19ef3fed81f9f6297f430173b0
oscar <oscarsilla00@gmail.com>
2025-02-24 19:12:05 +0100

    feat: Toolbar sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    ToolbarComponent,
-  ],
+  imports: [CommonModule, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
