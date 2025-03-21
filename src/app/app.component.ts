ea0a2c96e18cd51abd32a1aace7076661805bea4
oscar <oscarsilla00@gmail.com>
2025-03-21 23:00:23 +0100

    feat: Redirect to registration form if there isn't token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-personal-book';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (!localStorage.getItem('token')) {
+      this.router.navigate(['/registro']);
+    }
+  }
 }
