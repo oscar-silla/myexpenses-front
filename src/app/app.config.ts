66573937e6f2d1222e094638e67d6e0fcb59e9b7
oscar <oscarsilla00@gmail.com>
2025-04-11 16:24:48 +0200

    feat: Registration process

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     provideAnimationsAsync(),
+    provideAnimations(),
   ],
 };
--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RegistrationFormComponent } from '../../components/registration-form/registration-form.component';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { CommonModule } from '@angular/common';
+import 'animate.css';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [RegistrationFormComponent],
+  imports: [RegistrationFormComponent, LoginFormComponent, CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css',
 })
-export class RegistrationComponent {}
+export class RegistrationComponent {
+  protected isLoginMode: boolean = false;
+
+  protected toggleLoginMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
