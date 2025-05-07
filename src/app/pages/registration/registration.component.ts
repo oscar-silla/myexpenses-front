9bbf23503ecf698121ff29187683331d25ac56ad
oscar <oscarsilla00@gmail.com>
2025-05-07 22:02:40 +0200

    feat: Remove animate css

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RegistrationFormComponent } from '../../components/registration-form/registration-form.component';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 import { CommonModule } from '@angular/common';
-import 'animate.css';
 import { VerificationFormComponent } from '../../components/verification-form/verification-form.component';
 
 @Component({
